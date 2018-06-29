092dd0296a933d273e136831fa744b9f3c4f883f
junxia chen <junxiachen@junxiadeMacBook-Pro.local>
2018-06-29 18:23:43 +0800

    feat: tpl

---
+++ reactTpl/app/component/index.js
@@ -0,0 +1,15 @@
+'use strict'
+import React from 'react'
+import style from './css.css'
+class App extends React.Component {
+ 
+  render() {
+    return (
+      <div>
+        react tpl
+      </div>
+    )
+  }
+}
+
+export default App
