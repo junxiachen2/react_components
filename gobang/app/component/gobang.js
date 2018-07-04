4535499f9f8b371f041cf18a0db8272b277085ee
junxia chen <junxiachen@junxiadeMacBook-Pro.local>
2018-07-04 18:20:18 +0800

    fix: gobang

---
+++ gobang/app/component/gobang.js
@@ -0,0 +1,111 @@
+const defaultObj = {
+  // player: 1,
+  row: 15,
+  column: 15
+}
+class ChessBoard {
+  constructor (obj) {
+    this.obj = Object.assign(defaultObj, obj)
+    this.mappingArr = [] // 0代表没有走过，玩家1，玩家2
+
+    this.reset()
+  }
+  reset () {
+    this.mappingBoard()
+  }
+  // 映射棋盘
+  mappingBoard () {
+    const { mappingArr } = this
+    const { row, column } = this.obj
+    for (let i = 0; i < column; i++) {
+      mappingArr[i] = []
+      for (let j = 0; j < row; j++) {
+        mappingArr[i][j] = 0
+      }
+    }
+    this.mappingArr = mappingArr
+  }
+  // 下子
+  putChess (player, x, y) {
+    const { mappingArr } = this
+    if (mappingArr[x][y]) return
+    this.mappingArr[x][y] = player
+    // console.log(`玩家${player}在(${x},${y})下子`)
+    return true
+  }
+  // 通过下子位置判断输赢
+  judge (player, x, y) {
+    let winFlag
+    const { mappingArr } = this
+    // 判断水平方向是否胜利
+    for (let i = 0; i < 5; i++) {
+      if (x - i < 0) continue
+      winFlag = true
+      for (let j = 0; j < 5; j++) {
+        if (x - i + j > 14) {
+          winFlag = false
+        }
+        else if (mappingArr[x - i + j][y] !== player) {
+          winFlag = false
+        }
+      }
+      if (winFlag) {
+        return true
+      }
+    }
+
+    // 判断竖直方向是否胜利
+    for (let i = 0; i < 5; i++) {
+      if (y - i < 0) continue
+      winFlag = true
+      for (let j = 0; j < 5; j++) {
+        if (y - i + j > 14) {
+          winFlag = false
+        }
+        else if (mappingArr[x][y - i + j] !== player) {
+          winFlag = false
+        }
+      }
+      if (winFlag) {
+        return true
+      }
+    }
+
+    // 判断斜方向(左上到右下)
+    for (let i = 0; i < 5; i++) {
+      if (x - i < 0 || y - i < 0) continue
+      winFlag = true
+      for (let j = 0; j < 5; j++) {
+        if (x - i + j > 14 || y - i + j > 14) {
+          winFlag = false
+        }
+        else if (mappingArr[x - i + j][ y - i + j] !== player) {
+          winFlag = false
+        }
+      }
+      if (winFlag) {
+        return true
+      }
+    }
+
+    // 判断斜方向(右上到左下)
+    for (let i = 0; i < 5; i++) {
+      if (x + i > 14 || y - i < 0) continue
+      winFlag = true
+      for (let j = 0; j < 5; j++) {
+        if (x + i - j < 0 || y - i + j > 14) {
+          winFlag = false
+        }
+        else if (mappingArr[x + i - j][y - i + j] !== player) {
+          winFlag = false
+        }
+      }
+      if (winFlag) {
+        return true
+      }
+    }
+    return false
+  }
+}
+
+export default ChessBoard
--- gobang/app/component/index.js
+++ gobang/app/component/index.js
@@ -1,39 +1,87 @@
 'use strict'
 import React from 'react'
 import style from './css.css'
-import ChessBoard from './class/gobang'
+import ChessBoard from './gobang'
+const cbConfig = {
+  padding: 10,
+  row: 15,
+  column: 15,
+  size: 20,
+  radius: 6
+}
 class App extends React.Component {
+  constructor () {
+    super()
+    this.chessboard = null
+    this.canvasDom = null
+    this.player = 1 // 玩家1，玩家2
+    this.winFlag = false
+  }
   componentDidMount () {
-    this.chessboard = new ChessBoard(this.mainCanvas, this.bgCanvas, {})
-    // this.chessboard.addEventListener('click', this._putChess)
+    this.chessboard = new ChessBoard()
   }
   _putChess (e) {
-    console.log(e.target.value)
-    if (this.chessboard) this.chessboard._toPutChess(e.target)
+    if (this.winFlag) return
+    const { chessboard, player } = this
+    const { pageX, pageY } = e
+    const { top, left } = this.canvasDom.getBoundingClientRect()
+    const { padding, size } = cbConfig
+    const x = Math.floor((pageX - left - padding / 2) / size)
+    const y = Math.floor((pageY - top - padding / 2) / size)
+    const canChess = chessboard.putChess(player, x, y)
+    if (!canChess) return
+    this._drawChess(player, x, y)
+    this.winFlag = chessboard.judge(player, x, y)
+    if (this.winFlag) {
+      console.log(`玩家${player}胜利！`)
+    }
+    this.player = player === 1 ? 2 : 1
+  }
+  _drawChess (player, x, y) {
+    const { padding, size, radius } = cbConfig
+    const ctx = this.canvasDom.getContext('2d')
+    const drawX = padding + x * size
+    const drawY = padding + y * size
+    ctx.beginPath()
+    ctx.arc(drawX, drawY, radius, 0, Math.PI * 2, false)
+    const g = ctx.createRadialGradient(drawX, drawY, radius, drawX, drawY, 0)
+    if (player === 1) {
+      g.addColorStop(0, '#000')
+      g.addColorStop(1, '#636766')
+    }
+    else if (player === 2) {
+      g.addColorStop(0, '#D1D1D1')
+      g.addColorStop(1, '#fff')
+    }
+    ctx.fillStyle = g
+    ctx.fill()
+  }
+  _drawBg (res) {
+    const bgSelector = res
+    const { row, column, size, padding } = cbConfig
+    const ctx = bgSelector.getContext('2d')
+    // 列
+    for (let i = 0; i < row; i++) {
+      ctx.beginPath()
+      ctx.moveTo(padding + i * size, padding)
+      ctx.lineTo(padding + i * size, padding + size * 14)
+      ctx.stroke()
+    }
+    // 行
+    for (let i = 0; i < column; i++) {
+      ctx.beginPath()
+      ctx.moveTo(padding, padding + i * size)
+      ctx.lineTo(padding + size * (column - 1), padding + i * size)
+      ctx.stroke()
+    }
   }
   render () {
     return (
       <div>
         <div className={style.container}>
-          <canvas
-            ref={res => {
-              this.mainCanvas = res
-            }}
-            className={style.gobangBg}
-            width="300"
-            height="300"
-          />
-          <canvas
-            ref={res => {
-              this.bgCanvas = res
-            }}
-            className={style.gobangMain}
-            width="300"
-            height="300"
-            onClick={e => {
-              this._putChess(e)
-            }}
-          >
+          <canvas ref={res => { this._drawBg(res) }} className={style.gobangBg} width="300" height="300"/>
+          <canvas ref={res => { this.canvasDom = res }} className={style.gobangMain} width="300" height="300"
+            onClick={e => { this._putChess(e) }}>
             你的浏览器不支持canvas,请用其他浏览器打开~
           </canvas>
         </div>
